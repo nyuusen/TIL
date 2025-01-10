8ee114caf530d8c0d8fe669fc0989e6b9f13b345
nyuusen <pppike.0131@gmail.com>
2025-01-10 22:50:32 +0900

    yyyymmddでコミットするシェルを追加

---
+++ commit_with_date.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 現在の日付を取得して、yyyyMMdd形式にフォーマット
+current_date=$(date +%Y%m%d)
+
+# コミットするファイルをすべてステージング
+git add . 
+
+# コミットメッセージに日付を使用
+git commit -m "$current_date"
+
+# コミットが成功した場合のメッセージ
+if [ $? -eq 0 ]; then
+    echo "Committed with message: $current_date"
+else
+    echo "Failed to commit. Please check for errors."
+fi
