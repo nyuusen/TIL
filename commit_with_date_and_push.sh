256e9d6847fdf7c85145a60e943da940ba878c23
nyuusen <pppike.0131@gmail.com>
2025-01-19 16:50:55 +0900

    20250119

--- commit_with_date_and_push.sh
+++ commit_with_date_and_push.sh
@@ -2,16 +2,12 @@
 
 git pull
 
-# 現在の日付を取得して、yyyyMMdd形式にフォーマット
 current_date=$(date +%Y%m%d)
 
-# コミットするファイルをすべてステージング
 git add . 
 
-# コミットメッセージに日付を使用
 git commit -m "$current_date"
 
-# コミットが成功した場合のメッセージ
 if [ $? -eq 0 ]; then
     echo "Committed with message: $current_date"
 else
