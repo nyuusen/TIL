4d413a641710eb5691e9f2e2a8a46f9fd455fe32
nyuusen <pppike.0131@gmail.com>
2025-09-18 08:09:26 +0900

    20250918

--- commit_with_date_and_push.sh
+++ commit_with_date_and_push.sh
@@ -6,12 +6,13 @@ current_date=$(date +%Y%m%d)
 
 git add . 
 
-git commit -m "$current_date"
+git commit -m "$current_date" 2>error.log
 
 if [ $? -eq 0 ]; then
     echo "Committed with message: $current_date"
 else
     echo "Failed to commit. Please check for errors."
+    cat error.log
 fi
 
 git push origin main
